bb93ac95fa69f17fb034688d8f1d6111a897aff7
Craters <adryan.casineanu99@gmail.com>
2023-03-15 21:23:01 +0100

    Stamina

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,48 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    //public Transform groundCheck;
-    //public LayerMask groundLayer;
+    public float runSpeed = 10f;
+    public float maxStamina = 100f;
+    public float staminaRegenRate = 10f;
+    public float staminaDepletionRate = 20f;
+    public Slider staminaSlider;
+    public float staminaCooldown = 2f;
 
     private Rigidbody rb;
-    //private bool isGrounded;
+    private Vector3 moveDirection;
+    private bool isRunning;
+    private float currentStamina;
 
-    // Start is called before the first frame update
+    private bool isCooldown;
+    private float currentCooldownTime;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentStamina = maxStamina;
+        UpdateStaminaUI();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
-        rb.velocity = movement.normalized * moveSpeed;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-       /* isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+        moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }*/
+            if (currentStamina > 0)
+            {
+                isRunning = true;
+            }
+        }
 
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        
+        float speed = moveSpeed;
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (isRunning && currentStamina > 0)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            speed = runSpeed;
+            currentStamina -= staminaDepletionRate * Time.fixedDeltaTime;
+            UpdateStaminaUI();
         }
+        else
+        {
+            currentStamina += staminaRegenRate * Time.fixedDeltaTime;
+            UpdateStaminaUI();
+        }
+
+        if (isCooldown)
+        {
+            currentCooldownTime -= Time.fixedDeltaTime;
+
+            if (currentCooldownTime <= 0f)
+            {
+                isCooldown = false;
+            }
+        }
+
+        if (currentStamina <= 0f)
+        {
+            isCooldown = true;
+            currentCooldownTime = staminaCooldown;
+        }
+
+        currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+
+        rb.MovePosition(rb.position + transform.TransformDirection(moveDirection) * speed * Time.fixedDeltaTime);
+    }
+
+    void UpdateStaminaUI()
+    {
+        staminaSlider.value = currentStamina / maxStamina;
     }
 }
 
