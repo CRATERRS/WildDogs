88d6b4f6f23cb8570667ca8c2921a15ce75b345b
Craters <adryan.casinenau99@gmail.com>
2023-03-16 11:52:07 +0100

    gunsystem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,15 +21,33 @@ public class PlayerController : MonoBehaviour
     private bool isCooldown;
     private float currentCooldownTime;
 
+    //CameraContrller
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] bool lockCursor = true;
+
+    float cameraPitch = 0.0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         currentStamina = maxStamina;
         UpdateStaminaUI();
+
+        //Cursor
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     void Update()
     {
+        //Camera
+        UpdateMouseLook();
+
+
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -89,6 +107,19 @@ public class PlayerController : MonoBehaviour
     void UpdateStaminaUI()
     {
         staminaSlider.value = currentStamina / maxStamina;
+    }
+
+    void UpdateMouseLook()
+    {
+       // Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        //cameraPitch -= mouseDelta.y * mouseSensitivity;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        //transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
     }
 }
 
